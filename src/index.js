3a3877c543e6ddf3e32e0dfb3d345ee9803ccd79
Mohamed Aboulmagd <aboulmagd@live.com>
2020-07-04 18:46:07 +0300

    Understanding React Class Components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Employee extends React.Component {
+  // the props inside component is read only
+  constructor(props){
+    super(props);
+    console.log(props);
+  }
+  render() {
+    return <div>
+      <h2>Employee Details.....</h2>
+      <p>
+        <label>Employee ID : <b>{this.props.Id}</b> </label>
+      </p>
+      <p>
+        <label>Employee Name : <b>{this.props.Name}</b> </label>
+      </p>
+      <p>
+        <label>Employee Location : <b>{this.props.Location}</b> </label>
+      </p>
+      <p>
+        <label>Employee Salary: <b>{this.props.Salary}</b> </label>
+      </p>
+      <Department DeptName={this.props.DeptName} HeadName={this.props.HeadName}></Department>
+    </div>
+  }
+}
+
+class Department extends React.Component {
+
+  render(){
+    return <div>
+      <h2>Department Details...</h2>
+      <p>
+        <label>Dept Name : <b>{this.props.DeptName}</b></label>
+      </p>
+      <p>
+        <label>Head Name : <b>{this.props.HeadName}</b></label>
+      </p>
+      </div>;
+  }
+}
+
+const element=<Employee Id="101" Name="aboulmagd" Location="saudi arabia" Salary="500" DeptName="Dev" HeadName="Technologies"></Employee>
+ReactDOM.render(element, document.getElementById('root'));
